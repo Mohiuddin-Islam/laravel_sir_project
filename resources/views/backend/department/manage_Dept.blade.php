04332e3845efffb3f29adbd6b1cfac9783df2695
masudrana417 <masudrana71@gmail.com>
2024-12-03 12:57:12 +0600

    03 Dec 2024

--- resources/views/backend/department/manage_Dept.blade.php
+++ resources/views/backend/department/manage_Dept.blade.php
@@ -38,138 +38,164 @@
     <script src="{{ asset('dist/js/dropdown-bootstrap-extended.js') }}"></script>
     <!-- Init JavaScript -->
     <script src="{{ asset('dist/js/init.js') }}"></script>
+    <script>
+        $(document).ready(function() {
+            $("#btnAdd").click(function(e) {
+                e.preventDefault();
+                // alert("you have clicked");
+                let mydata = $("#deptForm").serialize();
+                //alert(data);
+                $.ajax({
+                    url: "{{route('department.store')}}" ,
+                    type: "POST",
+                    data:{
+                        "_token": "{{ csrf_token()}}",
+                        "data": mydata
+                    },
+                    success: function(result) {
+                        alert(result.success);
+                        //$("#div1").html(result);
+                    }
+                });
+            })
+        });
+    </script>
 @endsection
 
+
+
 <h3>List of All Specialist</h3>
+@section('content')
 
-<div class="container-fluid">
+    <div class="container-fluid">
 
-    <!-- Title -->
-    <div class="row heading-bg bg-green">
-        <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
-            <h5 class="txt-light">Export</h5>
+        <!-- Title -->
+        <div class="row heading-bg bg-green">
+            <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
+                <h5 class="txt-light">Export</h5>
+            </div>
+            <!-- Breadcrumb -->
+            <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
+                <ol class="breadcrumb">
+                    <li><a href="index.html">Dashboard</a></li>
+                    <li><a href="#"><span>table</span></a></li>
+                    <li class="active"><span>Export</span></li>
+                </ol>
+            </div>
+            <!-- /Breadcrumb -->
         </div>
-        <!-- Breadcrumb -->
-        <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
-            <ol class="breadcrumb">
-                <li><a href="index.html">Dashboard</a></li>
-                <li><a href="#"><span>table</span></a></li>
-                <li class="active"><span>Export</span></li>
-            </ol>
-        </div>
-        <!-- /Breadcrumb -->
-    </div>
-    <!-- /Title -->
+        <!-- /Title -->
 
 
-    <!-- Row -->
-    <div class="row">
-        <div class="col-sm-12">
-            <div class="panel panel-default card-view">
-                <div class="panel-heading">
-                    <div class="pull-left">
-                        @if (session('msg'))
-                            <div class="alert alert-success">{{ session('msg') }}</div>
-                        @endif
-                        <h6 class="panel-title txt-dark">Export</h6>
-                        <div class="pull-right">
-                            <button class="btn btn-info" data-toggle="modal" data-target="#responsive-modal">Create New
-                                Department</button>
-                            {{-- <a class="btn btn-warning" href="{{ route('department.create') }}"> Create New
+        <!-- Row -->
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="panel panel-default card-view">
+                    <div class="panel-heading">
+                        <div class="pull-left">
+                            @if (session('msg'))
+                                <div class="alert alert-success">{{ session('msg') }}</div>
+                            @endif
+
+                            <div class="pull-right">
+                                <button class="btn btn-info" data-toggle="modal" data-target="#responsive-modal">Create New
+                                    Department</button>
+                                {{-- <a class="btn btn-warning" href="{{ route('department.create') }}"> Create New
                                 Department</a> --}}
+                            </div>
                         </div>
+                        <div class="clearfix"></div>
                     </div>
-                    <div class="clearfix"></div>
-                </div>
-                <div class="panel-wrapper collapse in">
-                    <div class="panel-body">
-                        <div class="table-wrap">
-                            <div class="table-responsive">
-                                <table id="example" class="table table-hover display  pb-30">
-                                    <thead>
-                                        <tr>
-                                            <th>ID</th>
-                                            <th>Department Name</th>
-                                            <th>Details</th>
-                                            <th>Action</th>
-
-                                        </tr>
-                                    </thead>
-                                    <tfoot>
-                                        <tr>
-                                            <th>ID</th>
-                                            <th>Department Name</th>
-                                            <th>Details</th>
-                                            <th style="width:280px">Action</th>
-
-                                        </tr>
-                                    </tfoot>
-                                    <tbody>
-                                        @foreach ($departments as $item)
+                    <div class="panel-wrapper collapse in">
+                        <div class="panel-body">
+                            <div class="table-wrap">
+                                <div class="table-responsive">
+                                    <table id="example" class="table table-hover display  pb-30">
+                                        <thead>
                                             <tr>
-                                                <td>{{ $loop->iteration }}</td>
-                                                <td>{{ $item->name }}</td>
-                                                <td>{{ $item->details }}</td>
-                                                <td>
-
-                                                    <form action="{{ route('department.destroy', $item->id) }}"
-                                                        method="POST">
-
-                                                        <a class="btn btn-primary"
-                                                            href="{{ route('department.edit', $item->id) }}">Edit</a>
-                                                        <a class="btn btn-success"
-                                                            href="{{ route('department.show', $item->id) }}">View</a>
-                                                        @csrf
-                                                        @method('DELETE')
-                                                        <button type="submit" class="btn btn-danger">Delete</button>
-
-                                                    </form>
-
-                                                </td>
+                                                <th>ID</th>
+                                                <th>Department Name</th>
+                                                <th>Details</th>
+                                                <th>Action</th>
 
                                             </tr>
-                                        @endforeach
+                                        </thead>
+                                        <tfoot>
+                                            <tr>
+                                                <th>ID</th>
+                                                <th>Department Name</th>
+                                                <th>Details</th>
+                                                <th style="width:280px">Action</th>
 
-                                    </tbody>
-                                </table>
+                                            </tr>
+                                        </tfoot>
+                                        <tbody>
+                                            @foreach ($departments as $item)
+                                                <tr>
+                                                    <td>{{ $loop->iteration }}</td>
+                                                    <td>{{ $item->name }}</td>
+                                                    <td>{{ $item->details }}</td>
+                                                    <td>
+
+                                                        <form action="{{ route('department.destroy', $item->id) }}"
+                                                            method="POST">
+
+                                                            <a class="btn btn-primary"
+                                                                href="{{ route('department.edit', $item->id) }}">Edit</a>
+                                                            <a class="btn btn-success"
+                                                                href="{{ route('department.show', $item->id) }}">View</a>
+                                                            @csrf
+                                                            @method('DELETE')
+                                                            <button type="submit" class="btn btn-danger">Delete</button>
+
+                                                        </form>
+
+                                                    </td>
+
+                                                </tr>
+                                            @endforeach
+
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+        <!-- /Row -->
     </div>
-    <!-- /Row -->
-</div>
 
-<!-- sample modal content -->
-<!-- /.modal -->
-<div id="responsive-modal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
-    aria-hidden="true" style="display: none;">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                <h5 class="modal-title">Modal Content is Responsive</h5>
-            </div>
-            <div class="modal-body">
-                <form>
-                    <div class="form-group">
-                        <label for="recipient-name" class="control-label mb-10">Department Name:</label>
-                        <input type="text" class="form-control" id="recipient-name">
-                    </div>
-                    <div class="form-group">
-                        <label for="message-text" class="control-label mb-10">Details:</label>
-                        <textarea class="form-control" id="message-text"></textarea>
-                    </div>
-                </form>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-danger">Save changes</button>
+    <!-- sample modal content -->
+    <!-- /.modal -->
+    <div id="responsive-modal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+        aria-hidden="true" style="display: none;">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                    <h5 class="modal-title">Modal Content is Responsive</h5>
+                </div>
+                <div class="modal-body">
+                    <form id="deptForm">
+                        <div class="form-group">
+                            <label for="recipient-name" class="control-label mb-10">Department Name:</label>
+                            <input type="text" class="form-control" name="name" id="recipient-name">
+                        </div>
+                        <div class="form-group">
+                            <label for="message-text" class="control-label mb-10">Details:</label>
+                            <textarea class="form-control" name="details" id="message-text"></textarea>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <button type="button" id="btnAdd" class="btn btn-danger">Add</button>
+                </div>
             </div>
         </div>
     </div>
-</div>
-<!-- Button trigger modal -->
+    <!-- Button trigger modal -->
+
 @endsection
--- resources/views/backend/specialist/create.blade.php
+++ resources/views/backend/specialist/create.blade.php
@@ -65,7 +65,6 @@
 
     <div class="row">
         
-        
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default card-view">
                 <div class="panel-heading">
